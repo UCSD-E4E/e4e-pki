b99c0af291691dd9dc2a0553fbead7e481f8895a
Nathan Hui <nthui@ucsd.edu>
2025-03-25 18:24:36 -0700

    feat: Adds signing tools

---
+++ tools/sign_tls_server_request.py
@@ -0,0 +1,21 @@
+'''
+Signs a TLS Server request
+'''
+import subprocess
+from pathlib import Path
+
+
+def main():
+    csr = Path(input('Path to Certificate Signing Request: '))
+    if not csr.exists():
+        print('Invalid path!')
+        return
+    output_path = csr.with_suffix('.crt')
+    cmd = ['openssl', 'ca', '-config', 'etc/tls-ca.conf', '-in',
+           csr.as_posix(), '-out', output_path.as_posix(), '-extensions',  'server_ext']
+    subprocess.check_call(cmd)
+
+    print(f'Signed Certificate at {output_path.as_posix()}')
+
+if __name__ == '__main__':
+    main()
