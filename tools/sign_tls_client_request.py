079c312c8a09981ab5324c874a0297702ba7f99a
Nathan Hui <nthui@ucsd.edu>
2025-05-05 09:49:31 -0700

    feat: creates TLS client support

---
+++ tools/sign_tls_client_request.py
@@ -0,0 +1,21 @@
+'''
+Signs a TLS Client request
+'''
+import subprocess
+from pathlib import Path
+
+
+def main():
+    csr = Path(input('Path to Certificate Signing Request: '))
+    if not csr.exists():
+        print('Invalid path!')
+        return
+    output_path = (Path('certs') / csr.name).with_suffix('.crt')
+    cmd = ['openssl', 'ca', '-config', 'etc/signing-ca.conf', '-in',
+           csr.as_posix(), '-out', output_path.as_posix(), '-extensions',  'client_ext', '-policy', 'extern_pol']
+    subprocess.check_call(cmd)
+
+    print(f'Signed Certificate at {output_path.as_posix()}')
+
+if __name__ == '__main__':
+    main()
