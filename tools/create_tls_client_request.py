c31efbd44a269ea96a6162cf73b9610128fabd45
Nathan Hui <nthui@ucsd.edu>
2025-05-05 09:57:58 -0700

    fix: Updates variable names

--- tools/create_tls_client_request.py
+++ tools/create_tls_client_request.py
@@ -6,22 +6,18 @@ import os
 
 
 def main():
-    hostnames = []
-    while True:
-        hostname = input('Cert file name: ')
-        if hostname == '':
-            if len(hostnames) == 0:
-                print('Cert file name is required!')
-                continue
-            break
-        hostnames.append(hostname)
-        break
+    cert_name = None
+    while cert_name is None or cert_name == '':
+        cert_name = input('Cert file name: ')
+        if cert_name == '':
+            print('Cert file name is required!')
+            continue
 
     subprocess.check_call(['openssl', 'req', '-new', '-config', 'etc/tls-client.conf',
-                          '-out', f'certs/{hostnames[0]}.csr', '-keyout', f'certs/{hostnames[0]}.key'])
+                          '-out', f'certs/{cert_name[0]}.csr', '-keyout', f'certs/{cert_name[0]}.key'])
 
-    print(f'Certificate signing request at certs/{hostnames[0]}.csr')
-    print(f'New private key at certs/{hostnames[0]}.key')
+    print(f'Certificate signing request at certs/{cert_name[0]}.csr')
+    print(f'New private key at certs/{cert_name[0]}.key')
 
 
 if __name__ == '__main__':
