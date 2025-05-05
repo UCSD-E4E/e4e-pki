2d8e4fe8914b1e5d77ade022402f0055ebe0fbfb
Nathan Hui <nthui@ucsd.edu>
2025-05-05 09:55:32 -0700

    fix: Updates cert filename

--- tools/create_tls_client_request.py
+++ tools/create_tls_client_request.py
@@ -8,19 +8,17 @@ import os
 def main():
     hostnames = []
     while True:
-        hostname = input('Hostname (press Enter to continue): ')
+        hostname = input('Cert file name: ')
         if hostname == '':
             if len(hostnames) == 0:
-                print('At least one hostname is required!')
+                print('Cert file name is required!')
                 continue
             break
         hostnames.append(hostname)
-
-    new_environ = os.environ.copy()
-    new_environ['SAN'] = ','.join(f'DNS:{hostname}' for hostname in hostnames)
+        break
 
     subprocess.check_call(['openssl', 'req', '-new', '-config', 'etc/tls-client.conf',
-                          '-out', f'certs/{hostnames[0]}.csr', '-keyout', f'certs/{hostnames[0]}.key'], env=new_environ)
+                          '-out', f'certs/{hostnames[0]}.csr', '-keyout', f'certs/{hostnames[0]}.key'])
 
     print(f'Certificate signing request at certs/{hostnames[0]}.csr')
     print(f'New private key at certs/{hostnames[0]}.key')
